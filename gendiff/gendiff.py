8cab2f5dada2e3bf80b8f99f8a610807bf04b037
Evchik <elcric66@gmail.com>
2021-09-21 21:06:35 +0300

    step 3

---
+++ gendiff/gendiff.py
@@ -0,0 +1,42 @@
+import json
+
+import pytest
+
+# @pytest.fixture
+# def file_path1():
+#     return '/home/soleny/python-project-lvl2/gendiff/file1.json'
+
+
+# @pytest.fixture
+# def file_path2():
+#     return '/home/soleny/python-project-lvl2/gendiff/file2.json'
+
+
+def generate_diff(file_path1, file_path2):
+    first_file = json.load(open(file_path1))
+    second_file = json.load(open(file_path2))
+    contains_only_in_first = first_file.keys() - second_file.keys()
+    contains_only_in_second = second_file.keys() - first_file.keys()
+    contains_in_both_files = first_file.keys() & second_file.keys()
+    differense = []
+    for key in contains_only_in_first:
+      differense.append(f'  - {key}: {first_file[key]}')
+    for key in contains_only_in_second:
+      differense.append(f'  + {key}: {second_file[key]}')
+    for key in contains_in_both_files:
+      if first_file[key] == second_file[key]:
+        differense.append(f'    {key}: {first_file[key]}')
+      else:
+        differense.append(f'  - {key}: {first_file[key]}')
+        differense.append(f'  + {key}: {second_file[key]}')
+    sorted_differense = sorted(differense, key = lambda x: x[4])
+    result = '{\n' + '\n'.join(sorted_differense) + '\n}'
+    return result
+
+    
+    
+
+ 
+    
+
+
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,12 +1,17 @@
 import argparse
+import pathlib
+from gendiff.gendiff import generate_diff
+
 
 def main():
-    pass
+    parser = argparse.ArgumentParser(description='Generate diff', conflict_handler='resolve')
+    parser.add_argument('first_file', type=pathlib.Path)
+    parser.add_argument('second_file', type=pathlib.Path)
+    parser.add_argument('-f', '--format',  metavar='FORMAT',  help='set format of output')
+    args = parser.parse_args()
+    print(generate_diff(args.first_file, args.second_file))
 
-parser = argparse.ArgumentParser(description='Generate diff', conflict_handler='resolve')
-parser.add_argument('first_file', type=open)
-parser.add_argument('second_file', type=open)
-args = parser.parse_args()
+
 
 if __name__ == '__main__':
     main()
