cab722b7dbfc8fe1aa04a7a0652480c6cd1b908a
Evchik <elcric66@gmail.com>
2021-10-05 20:32:28 +0300

    step 6 done

--- gendiff/format_difference.py
+++ gendiff/format_difference.py
@@ -1,39 +1,41 @@
 from gendiff.search_difference import is_dictionary, decode_value
-
+    
 
 def format(diff):
-    difference = ['{']
 
     def inner(diff, depth=0):
         if not is_dictionary(diff):
             return decode_value(diff)
 
         keys = sorted(diff.keys())
-        depth += 1
-        indent = "\t" * depth
-
+        indent = " " * depth
+        difference = ['{']
+        depth += 4
         for key in keys:
             if diff[key]['condition'] == 'deleted':
-                difference.append(f'{indent}- {key}: \
+                obj = (f'{indent}  - {key}: \
 {inner(diff[key]["value"], depth)}')
 
             elif diff[key]['condition'] == 'added':
-                difference.append(f'{indent}+ {key}: \
+                obj = (f'{indent}  + {key}: \
 {inner(diff[key]["value"], depth)}')
 
             elif diff[key]['condition'] == 'equal':
-                difference.append(f'{indent}  {key}: \
+                obj = (f'{indent}    {key}: \
 {inner(diff[key]["value"], depth)}')
 
             elif diff[key]['condition'] == 'replaced':
-                difference.append(f'{indent}- {key}: \
-{inner(diff[key]["value1"], depth)}')
-                difference.append(f'{indent}+ {key}: \
+                obj = (
+                f'{indent}  - {key}: \
+{inner(diff[key]["value1"], depth)}\n'
+                f'{indent}  + {key}: \
 {inner(diff[key]["value2"], depth)}')
 
-            elif diff[key]['condition'] == 'changed':
-                difference.append(f'{indent}  {key}: \
+            elif diff[key]['children']:
+                obj = (f'{indent}    {key}: \
 {inner(diff[key]["children"], depth)}')
-        difference.append('}')
+
+            difference.append(obj)
+        difference.append(f'{indent}}}')
         return '\n'.join(difference)
     return inner(diff)
--- gendiff/search_difference.py
+++ gendiff/search_difference.py
@@ -1,57 +1,63 @@
+from json import JSONEncoder
+
+
 def is_dictionary(object):
     return isinstance(object, dict)
 
 
 def decode_value(value):
-    if value is None:
-        return 'null'
+    if type(value) == bool or value is None:
+        return JSONEncoder().encode(value)
+    else:
+        return str(value)
 
-    elif type(value) == bool:
-        return str(value).lower()
-
-    return str(value)
 
 
 def get_keys(dict1, dict2):
     first_keys = list(dict1.keys())
     second_keys = list(dict2.keys())
-    return first_keys if dict1 == dict2 else set(first_keys + second_keys)
+    return set(first_keys + second_keys)
 
 
-def add_key(key, dict1, dict2, difference):
-    if key in dict1 and key not in dict2:
-        difference[key] = {
-                          'condition': 'deleted',
-                          'children': None,
-                          'value': search_difference(dict1[key]), }
+# def add_key(key, dict1, dict2, difference):
+#     if key in dict1 and key not in dict2:
+#         difference[key] = {
+#         'condition': 'deleted',
+#         'children': None,
+#         'value': search_difference(dict1[key]),
+#         }
 
-    elif key in dict2 and key not in dict1:
-        difference[key] = {
-                          'condition': 'added',
-                          'children': None,
-                          'value': search_difference(dict2[key]), }
+#     elif key in dict2 and key not in dict1:
+#         difference[key] = {
+#         'condition': 'added',
+#         'children': None,
+#         'value': search_difference(dict2[key]),
+#         }
 
-    else:
+#     else:
 
-        if dict1[key] == dict2[key]:
-            difference[key] = {
-                              'condition': 'equal',
-                              'children': None,
-                              'value': search_difference(dict1[key]), }
+#         if dict1[key] == dict2[key]:
+#             difference[key] = {
+#             'condition': 'equal',
+#             'children': None,
+#             'value': search_difference(dict1[key]),
+#             }
 
-        elif not is_dictionary(dict1[key]) or not is_dictionary(dict2[key]):
-            difference[key] = {
-            'condition': 'replaced',
-            'children': None,
-            'value1': search_difference(dict1[key]),
-            'value2': search_difference(dict2[key]), }
+#         elif not is_dictionary(dict1[key]) or not is_dictionary(dict2[key]):
+#             difference[key] = {
+#             'condition': 'replaced',
+#             'children': None,
+#             'value1': search_difference(dict1[key]),
+#             'value2': search_difference(dict2[key]),
+#             }
 
-        elif is_dictionary(dict1[key]) and is_dictionary(dict2[key]):
-            difference[key] = {
-                'condition': 'changed',
-                'children': (search_difference(dict1[key],
-                             search_difference(dict2[key]))),
-                'value': None, }
+#         elif is_dictionary(dict1[key]) and is_dictionary(dict2[key]):
+#             difference[key] = {
+#             'condition': 'changed',
+#             'children': (search_difference(dict1[key], dict2[key])),
+#             'value': None,
+#             }
+        
 
 
 def search_difference(dict1, dict2=None):
@@ -66,6 +72,42 @@ def search_difference(dict1, dict2=None):
     difference = {}
 
     for key in keys:
-        add_key(key, dict1, dict2, difference)
+        if key in dict1 and key not in dict2:
+            difference[key] = {
+            'condition': 'deleted',
+            'children': None,
+            'value': search_difference(dict1[key]),
+            }
+
+        elif key in dict2 and key not in dict1:
+            difference[key] = {
+            'condition': 'added',
+            'children': None,
+            'value': search_difference(dict2[key]),
+            }
+
+        else:
+
+            if dict1[key] == dict2[key]:
+                difference[key] = {
+                'condition': 'equal',
+                'children': None,
+                'value': search_difference(dict1[key]),
+                }
+
+            elif not is_dictionary(dict1[key]) or not is_dictionary(dict2[key]):
+                difference[key] = {
+                'condition': 'replaced',
+                'children': None,
+                'value1': search_difference(dict1[key]),
+                'value2': search_difference(dict2[key]),
+                }
+
+            elif is_dictionary(dict1[key]) and is_dictionary(dict2[key]):
+                difference[key] = {
+                'condition': 'changed',
+                'children': (search_difference(dict1[key], dict2[key])),
+                'value': None,
+                }
 
     return difference
