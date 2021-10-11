6124fa4e8259c7ebb7e40fed5ac5a50f5f51452b
Evchik <elcric66@gmail.com>
2021-10-11 23:31:08 +0300

    edit plain formater

--- gendiff/formaters/plain_formater.py
+++ gendiff/formaters/plain_formater.py
@@ -3,16 +3,27 @@ from json import JSONEncoder
 from gendiff.search_difference import is_dictionary
 
 
-def decode_value(value):
+def format_value(value):
+
+    """Returns formated value if it necessary"""
+
     if type(value) == bool or value is None:
         return JSONEncoder().encode(value)
     elif is_dictionary(value):
         return '[complex value]'
+    elif type(value) == int:
+        return f"{value}"
     else:
         return f"'{value}'"
 
 
 def format_plain(diff, path=''):
+
+    """Returns formated difference between two files if plain format
+
+    arguments:
+    diff: raw differene between two files
+    path: path to value (default: '') using to build full path"""
 
     keys = sorted(diff.keys())
     difference = []
@@ -25,18 +36,36 @@ def format_plain(diff, path=''):
 
 
 def get_formated_object(key, diff, full_path, condition):
+
+    """Make string in depending of value's condition
+
+    arguments:
+    key: current key in diff
+    diff: part of full difference (value)
+    full_path: full path to value
+    condition: condition of value in diff"""
+
     if condition == 'deleted':
         return (f"Property '{full_path}' was removed")
     elif condition == 'added':
         return (f"Property '{full_path}' "
-                f"was added with value: {decode_value(diff[key]['value'])}")
+                f"was added with value: {format_value(diff[key]['value'])}")
     elif condition == 'updated':
         return (f"Property '{full_path}' was updated. "
-                f"From {decode_value(diff[key]['value1'])} "
-                f"to {decode_value(diff[key]['value2'])}")
+                f"From {format_value(diff[key]['value1'])} "
+                f"to {format_value(diff[key]['value2'])}")
 
 
 def add_formated_object(key, diff, difference, full_path):
+
+    """Add formated object to formated difference in depending of it's condition
+
+    arguments:
+    key: current key in diff
+    diff: part of full difference (value)
+    difference: formated difference
+    full_path: full path to value"""
+
     if diff[key]['condition'] == 'not changed':
         obj = ''
     elif diff[key]['condition'] == 'changed':
