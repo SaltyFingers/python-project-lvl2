f37e44fc769beb631a7e835afb1483806b9e4a09
Evchik <elcric66@gmail.com>
2021-10-20 00:26:49 +0300

    edit json formater

--- gendiff/formaters/json_formater.py
+++ gendiff/formaters/json_formater.py
@@ -17,24 +17,21 @@ def format_json(diff):
     arguments:
     diff: raw differene between two files"""
 
-    if not is_dictionary(diff):
-        return encode_value(diff)
 
     keys = sorted(diff.keys())
     difference = ['{']
-    count = 0
 
     for key in keys:
-        add_formated_object(key, diff, difference)
-        count += 1
-        if count < len(keys):
-            difference.append(', ')
+        strings = map(lambda key: add_formated_object(
+                      key, diff), keys)
+        obj = ', '.join(strings)
 
+    difference.append(obj)
     difference.append('}')
     return ''.join(difference)
 
 
-def add_formated_object(key, diff, difference):
+def add_formated_object(key, diff):
 
     """Add formated object to formated difference in depending of it's condition
 
@@ -47,23 +44,43 @@ def add_formated_object(key, diff, difference):
 
     if condition == 'deleted':
         obj = (f'"{key}": {{"condition": "{condition}", "value": '
-               f'{format_json(diff[key]["value"])}}}')
+               f'{format_nested_object(diff[key]["value"])}}}')
 
     elif condition == 'added':
         obj = (f'"{key}": {{"condition": "{condition}", "value": '
-               f'{format_json(diff[key]["value"])}}}')
+               f'{format_nested_object(diff[key]["value"])}}}')
 
     elif condition == 'not changed':
         obj = (f'"{key}": {{"condition": "{condition}", "value": '
-               f'{format_json(diff[key]["value"])}}}')
+               f'{format_nested_object(diff[key]["value"])}}}')
 
     elif condition == 'updated':
         obj = (f'"{key}": {{"condition": "{condition}", "value1": '
-               f'{format_json(diff[key]["value1"])}, "value2": '
-               f'{format_json(diff[key]["value2"])}}}')
+               f'{format_nested_object(diff[key]["value1"])}, "value2": '
+               f'{format_nested_object(diff[key]["value2"])}}}')
 
     elif condition == 'nested':
         obj = (f'"{key}": {{"condition": "{condition}", "children": '
                f'{format_json(diff[key]["children"])}}}')
 
-    difference.append(obj)
+    return obj
+
+
+def format_nested_object(diff):
+
+    nested_diff = []
+    if is_dictionary(diff):
+        nested_diff.append('{')
+        keys = diff.keys()
+        count = 0
+        for key in keys:
+            obj = (f'"{key}":  "value": '
+                   f'{format_nested_object(diff[key])}}}')
+            nested_diff.append(obj)
+            count += 1
+            if count < len(keys):
+                nested_diff.append(', ')
+    else:
+        obj = encode_value(diff)
+        nested_diff.append(obj)
+    return ''.join(nested_diff)
